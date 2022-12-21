b9956b6fb993250033f2acbd353c47511bb1aeeb
Varshavt1999 <varshavt7399@gmail.com>
2022-12-21 12:50:46 +0530

    typography

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,22 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+export const MuiTypography = () => {
+  return (
+    <div>
+        <Typography variant='h4' component="h1" gutterBottom>Typography</Typography>
+        //-----------------------------------------------------------------------//
+        <Typography variant='h1' >Heading 1</Typography>
+        <Typography variant='h2' >Heading 2</Typography>
+        <Typography variant='h3' >Heading 3</Typography>
+        <Typography variant='h4' component="h1" gutterBottom>Heading 4</Typography>
+        <Typography variant='h5' >Heading 5</Typography>
+        <Typography variant='h6' >Heading 6</Typography>
+        <Typography variant='subtitle1' >Subtitle 1</Typography>
+        <Typography variant='subtitle2' >Subtitle 2</Typography>
+        <Typography variant='body1' gutterBottom>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Neque ornare aenean euismod elementum nisi quis eleifend quam adipiscing. Metus aliquam eleifend mi in. Nunc sed augue lacus viverra vitae. Sed tempus urna et pharetra pharetra massa. Congue nisi vitae suscipit tellus mauris a. Sagittis purus sit amet volutpat   </Typography>
+        <Typography variant='body2' > nunc. Tortor aliquam nulla facilisi cras fermentum odio eu. Et malesuada fames ac turpis egestas sed tempus urna et. ut pharetra sit amet aliquam id diam maecenas ultricies. Purus ut faucibus pulvinar elementum. Pellentesque diam volutpat commodo sed egestas egestas fringilla. Pellentesque dignissim enim sit amet venenatis urna cursus eget.
+Diam maecenas ultricies mi eget mauris pharetra et ultrices</Typography>
+    </div>
+  )
+}
